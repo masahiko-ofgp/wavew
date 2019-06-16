0fda45a30ca9e4274b2efacb470ca34dd55dc35c
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2019-06-16 14:21:41 +0900

    Add consts and struct MonoPcm

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,23 @@ use std::fs::File;
 use std::f32::consts::PI;
 use std::i16;
 
+const CHUNK_ID: i32 = 0x52494646;  // "RIFF"
+const FORMAT: i32 = 0x57415645;  // "WAVE"
+const SUB_CHUNK_1_ID: i32 = 0x666D7420;  // "fmt "
+const SUB_CHUNK_2_ID: i32 = 0x64617461;  // "data"
+
 
 fn main() -> std::io::Result<()> {
     let mut file = File::create("sine.wav")?;
     
-    let fs: i32 = 44100;
-    let bits: u16 = 16;
-    let length: u32 = (fs * 1) as u32;
+    let mut pcm = MonoPcm::new(44100_i32, 16_u16);
 
-    // Chunk ID 'RIFF'
-    file.write_i32::<BigEndian>(0x52494646)?;
+    file.write_i32::<BigEndian>(CHUNK_ID)?;
     // Chunk Size
     // 36 + SubChunk 2 Size
-    file.write_i32::<LittleEndian>(44136)?;
-    // Format 'WAVE'
-    file.write_i32::<BigEndian>(0x57415645)?;
-    // Sub Chunk 1 ID 'fmt '
-    file.write_i32::<BigEndian>(0x666D7420)?;
+    file.write_u32::<LittleEndian>(36_u32 + pcm.length)?;
+    file.write_i32::<BigEndian>(FORMAT)?;
+    file.write_i32::<BigEndian>(SUB_CHUNK_1_ID)?;
     // Sub Chunk 1 Size
     // PCM -> 16
     file.write_i32::<LittleEndian>(16)?;
@@ -31,7 +31,7 @@ fn main() -> std::io::Result<()> {
     file.write_u16::<LittleEndian>(1)?;
     // Sample Rate
     // 8000, 44100 ..
-    file.write_i32::<LittleEndian>(fs)?;
+    file.write_i32::<LittleEndian>(pcm.fs)?;
     // Byte Rate
     // SampleRate * NumChannels * BitsPerSample / 8
     file.write_i32::<LittleEndian>(88200)?;
@@ -39,31 +39,45 @@ fn main() -> std::io::Result<()> {
     // NumChannels * BitsPerSample / 8
     file.write_u16::<LittleEndian>(2)?;
     // Bits per sample
-    file.write_u16::<LittleEndian>(bits)?;
-    // Sub Chunk 2 ID 'data'
-    file.write_i32::<BigEndian>(0x64617461)?;
+    file.write_u16::<LittleEndian>(pcm.bits)?;
+    file.write_i32::<BigEndian>(SUB_CHUNK_2_ID)?;
     // Sub Chunk 2 Size
-    file.write_u32::<LittleEndian>(length)?;
+    file.write_u32::<LittleEndian>(pcm.length)?;
 
     let f0: f32 = 550.0;
     let amplitude = i16::MAX as f32;
-    
-    write_data(&mut file, f0, amplitude, length)?;
+
+    pcm.add_data(f0, amplitude);
+
+    for i in pcm.data.iter() {
+        file.write_i16::<LittleEndian>(*i)?;
+    }
 
     Ok(())
 }
 
+#[derive(Debug)]
+struct MonoPcm {
+    fs: i32,
+    bits: u16,
+    length: u32,
+    data: Vec<i16>,
+}
 
-// Data
-fn write_data(file: &mut File, f0: f32, amplitude: f32, length: u32)
-    -> std::io::Result<()>
-{
-    let sample = |t: f32| (t * f0 * 2.0 * PI).sin();
-
-    for t in (0 .. length).map(|x| x as f32 / 44100.0) {
-        file.write_i16::<LittleEndian>(
-            (sample(t) * amplitude) as i16
-            )?;
+impl MonoPcm {
+    fn new(fs: i32, bits: u16) -> MonoPcm {
+        MonoPcm {
+            fs: fs,
+            bits: bits,
+            length: (fs * 1) as u32,
+            data: Vec::new(),
+        }
     }
-    Ok(())
+    fn add_data(&mut self, f0: f32, amplitude: f32) {
+        let sample = |t: f32| (t * f0 * 2.0 * PI).sin();
+
+        for t in (0 .. self.length).map(|x| x as f32 / 44100.0) {
+            self.data.push((sample(t) * amplitude) as i16);
+        }
+    }
 }
