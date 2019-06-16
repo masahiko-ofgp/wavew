c052c6e7d142f75ff53972bbe750e49f89980e34
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2019-06-16 11:40:06 +0900

    Add fn write_data

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,67 @@ use std::fs::File;
 use std::f32::consts::PI;
 use std::i16;
 
-fn main() {
-    let mut file = File::create("sine.wav").expect("Error");
-    
-    // Chunk ID 'RIFF'
-    file.write_i32::<BigEndian>(0x52494646).unwrap();
-    // Chunk Size
-    file.write_i32::<LittleEndian>(88244).unwrap();
-    // Format 'WAVE'
-    file.write_i32::<BigEndian>(0x57415645).unwrap();
-    // Sub Chunk 1 ID 'fmt '
-    file.write_i32::<BigEndian>(0x666D7420).unwrap();
-    // Sub Chunk 1 Size
-    file.write_i32::<LittleEndian>(16).unwrap();
-    // Audio Format
-    file.write_u16::<LittleEndian>(1).unwrap();
-    // Num channels
-    file.write_u16::<LittleEndian>(2).unwrap();
-    // Sample Rate
-    file.write_i32::<LittleEndian>(44100).unwrap();
-    // Byte Rate
-    file.write_i32::<LittleEndian>(176400).unwrap();
-    // Block Align
-    file.write_u16::<LittleEndian>(4).unwrap();
-    // Bits per sample
-    file.write_u16::<LittleEndian>(16).unwrap();
-    // Sub Chunk 2 ID 'data'
-    file.write_i32::<BigEndian>(0x64617461).unwrap();
-    // Sub Chunk 2 Size
-    file.write_i32::<LittleEndian>(44100).unwrap();
 
-    // Data
-    for t in (0 ..44100).map(|x| x as f32 / 44100.0) {
-        let sample = (t * 500.0 * 2.0 * PI).sin();
-        let amplitude = i16::MAX as f32;
-        file.write_i16::<LittleEndian>((sample * amplitude) as i16).unwrap();
+fn main() -> std::io::Result<()> {
+    let mut file = File::create("sine.wav")?;
+    
+    let fs: i32 = 44100;
+    let bits: u16 = 16;
+    let length: u32 = (fs * 1) as u32;
+
+    // Chunk ID 'RIFF'
+    file.write_i32::<BigEndian>(0x52494646)?;
+    // Chunk Size
+    // 36 + SubChunk 2 Size
+    file.write_i32::<LittleEndian>(44136)?;
+    // Format 'WAVE'
+    file.write_i32::<BigEndian>(0x57415645)?;
+    // Sub Chunk 1 ID 'fmt '
+    file.write_i32::<BigEndian>(0x666D7420)?;
+    // Sub Chunk 1 Size
+    // PCM -> 16
+    file.write_i32::<LittleEndian>(16)?;
+    // Audio Format
+    // PCM = 1
+    file.write_u16::<LittleEndian>(1)?;
+    // Num channels
+    // Mono = 1, Stereo = 2
+    file.write_u16::<LittleEndian>(1)?;
+    // Sample Rate
+    // 8000, 44100 ..
+    file.write_i32::<LittleEndian>(fs)?;
+    // Byte Rate
+    // SampleRate * NumChannels * BitsPerSample / 8
+    file.write_i32::<LittleEndian>(88200)?;
+    // Block Align
+    // NumChannels * BitsPerSample / 8
+    file.write_u16::<LittleEndian>(2)?;
+    // Bits per sample
+    file.write_u16::<LittleEndian>(bits)?;
+    // Sub Chunk 2 ID 'data'
+    file.write_i32::<BigEndian>(0x64617461)?;
+    // Sub Chunk 2 Size
+    file.write_u32::<LittleEndian>(length)?;
+
+    let f0: f32 = 550.0;
+    let amplitude = i16::MAX as f32;
+    
+    write_data(&mut file, f0, amplitude, length)?;
+
+    Ok(())
+}
+
+
+// Data
+fn write_data(file: &mut File, f0: f32, amplitude: f32, length: u32)
+    -> std::io::Result<()>
+{
+    let sample = |t: f32| (t * f0 * 2.0 * PI).sin();
+
+    for t in (0 .. length).map(|x| x as f32 / 44100.0) {
+        file.write_i16::<LittleEndian>(
+            (sample(t) * amplitude) as i16
+            )?;
     }
+    Ok(())
 }
